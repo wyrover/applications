af6232151b4929772a188c5dbf0ebfdd056fb36e
lstables <lstables1@gmail.com>
2015-10-23 23:39:00 +0100

    updates

--- resources/views/references/settings.blade.php
+++ resources/views/references/settings.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-6" style="min-height: 200px; height: auto!important;">
+<div class="col-md-6" style="min-height: 200px!important; height: auto!important;">
 @foreach($settings as $field)
 
     <input type="hidden" name="id" value="{!! $field->id !!}">
