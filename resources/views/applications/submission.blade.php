f638ae5be088058db9db21d1a69028b34aefb321
lstables <lstables1@gmail.com>
2015-10-25 20:54:12 +0000

    bug fixes

--- resources/views/applications/submission.blade.php
+++ resources/views/applications/submission.blade.php
@@ -28,7 +28,7 @@
 
 <div class="container content">
     <div class="row">
-    <div class="col-lg-11 col-md-10 col-sm-11 col-xs-12">
+    <div class="col-lg-11 col-md-10 col-sm-11 col-xs-5">
          <p>Please fill out this application as accurately as possible make sure to fill as many boxes as you can</p>
         {!! Form::open(['url' => 'application', 'class' => 'form-horizontal']) !!}
 
