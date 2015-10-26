1645282eb1fe9c8bbbe8659e836330ca18d5fa0b
lstables <lstables1@gmail.com>
2015-10-26 20:08:30 +0000

    bug fixes & updates

--- app/References.php
+++ app/References.php
@@ -40,6 +40,6 @@ class References extends Model
      */
     public function application()
     {
-        return $this->hasOne('App\Applications');
+        return $this->hasOne('App\Applications', 'applications_id', 'id');
     }
 }
