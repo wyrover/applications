8a55fc941b9b53f5fc0a2f5afbd891b19689a215
lstables <lstables1@gmail.com>
2015-10-26 15:45:28 +0000

    bug fixes

--- app/Http/Controllers/ApplicationSubmissionController.php
+++ app/Http/Controllers/ApplicationSubmissionController.php
@@ -188,7 +188,7 @@ class ApplicationSubmissionController extends Controller
         $ref->completed = 'Yes';
         $ref->save();
 
-        $fields = Fields::create($request->except('_token','name','phone','position','email_address','applicant_started','date_left','reason_for_leaving'));
+        $fields = Fields::create($request->except('_token','name','phone','position','email_address','applicant_started','date_left','reason_for_leaving','code'));
         //$settings = Settings::create($request->only('label', 'label2', 'label3', 'label4', 'label5', 'label6', 'label7', 'label8', 'label9', 'label10', 'company_id'));
         $settings = Settings::where('company_id', Auth::user()->company_id)->first();
 
