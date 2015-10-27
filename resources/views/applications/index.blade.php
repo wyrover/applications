8f6b9f70439eacaa8f54fe8be01bf29a74e8b2de
lstables <lstables1@gmail.com>
2015-10-27 17:35:11 +0000

    bug fixes & updates

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -30,18 +30,15 @@
             <td>{!! $item->city !!}</td>
             <td><a href="/applications/export/application/{!! $item->id !!}" class="btn btn-sm btn-default"  data-toggle="tooltip" data-placement="top" title="Download Application"><i class="fa fa-download"></i> Download</a></td>
             <td>
-                {!! dd($item->reference()->first()->id) !!}
-            </td>
-            <td>
                 @if ($item->reference()->first()->completed == 'No' && ! empty($item->reference()->first()->referee_email))
                     <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
                 @else
-                    <a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    <a href="/applications/export/exportReferee/{!! $item->reference()->first()->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
                 @endif
                 @if ($item->reference()->first()->completedtwo == 'No' && ! empty($item->reference()->first()->referee_email2))
                     <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
                 @else
-                    <a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    <a href="/applications/export/exportRefereeTwo/{!! $item->reference()->first()->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
                 @endif
             </td>
             <td>
