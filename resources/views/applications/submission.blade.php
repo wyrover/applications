606f4786541691f48affbc704d7e098e826a5ba5
lstables <lstables1@gmail.com>
2015-11-03 12:15:19 +0000

    ...

--- resources/views/applications/submission.blade.php
+++ resources/views/applications/submission.blade.php
@@ -35,7 +35,7 @@
             <input type="hidden" name="company_id" value="{!! $company->id !!}">
             <input type="hidden" name="company_name_for_submission" value="{!! $company->name !!}">
             <input type="hidden" name="code" value="{!! str_random(40) !!}">
-            <input type="hidden" name="settings_id" value="">
+            <input type="hidden" name="settings_id" value="{!! $field->id !!}">
 
             <div class="panel panel-default">
                 <div class="panel-heading">Personal Details</div>
