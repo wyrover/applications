05f65eac7340eec0d570ac26b109439219ef6dee
lstables <lstables1@gmail.com>
2015-11-03 18:49:56 +0000

    ..

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -32,16 +32,19 @@
             <td>
                 @foreach ($refs as $ref)
 
-                    @if ($ref->completed == 'Yes')
-                        <button>Button1 ID: {{ $ref->id }}</button>
-                    @else
-                        <button>this is no button</button>
-                    @endif
+                    @if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')
 
-                    @if ($ref->completedtwo == 'Yes')
-                        <button>Button2 ID: {{ $ref->id }}</button>
+                        <a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+
+                    @elseif ($ref->completedtwo == 'Yes' && $ref->completed == 'No')
+
+                        <a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+
                     @else
-                        <button>this is no button</button>
+
+                        <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
+
+
                     @endif
 
                     {{--@if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')--}}
