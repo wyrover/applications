e71f367c39dcf81cf4fada8190fcd50bf82901c5
lstables <lstables1@gmail.com>
2015-11-03 19:49:16 +0000

    ..

--- resources/views/references/list.blade.php
+++ resources/views/references/list.blade.php
@@ -29,16 +29,31 @@
                 <td>{!! date('d/m/y', strtotime($ref->created_at)) !!}</td>
                 <td>{!! $ref->first_name !!}&nbsp;{!! $ref->last_name !!}</td>
                 <td>
-                    @if ($ref->completed == 'Yes')
-                        <a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-                    @else
-                        <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
-                    @endif
-                    @if ($ref->completedtwo == 'Yes')
-                        <a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-                    @else
-                        <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
-                    @endif
+                    {{--@if ($ref->completed == 'Yes')--}}
+                        {{--<a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+                    {{--@else--}}
+                        {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
+                    {{--@endif--}}
+                    {{--@if ($ref->completedtwo == 'Yes')--}}
+                        {{--<a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+                    {{--@else--}}
+                        {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
+                    {{--@endif--}}
+
+                        @if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')
+
+                            <a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+
+                        @elseif ($ref->completedtwo == 'Yes' && $ref->completed == 'No')
+
+                            <a href="/references/export/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+
+                        @else
+
+                            <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
+
+
+                        @endif
                 </td>
                 <td>
                     <a href="/references/{!! $ref->id !!}/delete" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to remove this?')"><i class="fa fa-trash"></i> Delete</a>
