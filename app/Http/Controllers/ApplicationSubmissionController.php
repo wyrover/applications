925d4ff558b939a27441fb8fe241dcd6c7477962
lstables <lstables1@gmail.com>
2015-11-03 14:57:30 +0000

    ..

--- app/Http/Controllers/ApplicationSubmissionController.php
+++ app/Http/Controllers/ApplicationSubmissionController.php
@@ -187,7 +187,7 @@ class ApplicationSubmissionController extends Controller
     public function postReference(Request $request)
     {
         $applicant = Applications::where('code', $request->segment(2))->first();
-        dd($applicant);
+        //dd($applicant);
         $settings = Settings::where('company_id', $applicant->company_id)->where('references_id', '=', 1)->get();
         $company = Company::where('id', $applicant->company_id)->first();
         $referee =  References::where('code', $request->segment(2))->first();
