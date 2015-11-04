eeb01b52c2ca9c3ab68f33005a3f6d805bf93042
lstables <lstables1@gmail.com>
2015-11-04 10:01:35 +0000

    updates

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -30,35 +30,35 @@
             <td>{!! $item->city !!}</td>
             <td><a href="/applications/export/application/{!! $item->id !!}" class="btn btn-sm btn-default"  data-toggle="tooltip" data-placement="top" title="Download Application"><i class="fa fa-download"></i> Download</a></td>
             <td>
-                @foreach ($refs as $ref)
-
-                    @if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')
-
-                        <a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-
-                    @elseif ($ref->completedtwo == 'Yes' && $ref->completed == 'No')
-
-                        <a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-
-                    @else
-
-                        <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
-
-
-                    @endif
+                {{--@foreach ($refs as $ref)--}}
 
                     {{--@if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')--}}
-                    {{--<a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+
+                        {{--<a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+
+                    {{--@elseif ($ref->completedtwo == 'Yes' && $ref->completed == 'No')--}}
+
+                        {{--<a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+
+                    {{--@else--}}
+
+                        {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
+
+
                     {{--@endif--}}
-                    {{--@if ($ref->completedtwo == 'Yes' && $ref->completed == 'No')--}}
-                    {{--<a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
-                    {{--@endif--}}
+
+                    @if ($ref->completed == 'Yes' && $ref->completedtwo == 'No')
+                    <a href="/applications/export/exportReferee/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    @endif
+                    @if ($ref->completedtwo == 'Yes' && $ref->completed == 'No')
+                    <a href="/applications/export/exportRefereeTwo/{!! $ref->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    @endif
 
                             {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
 
 
 
-                @endforeach
+                {{--@endforeach--}}
             </td>
             <td>
                 <a href="/applications/{!! $item->id !!}/notes" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="top" title="Notes"><i class="fa fa-edit"></i> Notes</a>
