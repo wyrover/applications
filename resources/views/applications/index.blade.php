ba25dd79d74983af65292a1fc3f9953ba4a30646
lstables <lstables1@gmail.com>
2015-11-04 10:14:16 +0000

    updates

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -49,14 +49,14 @@
 
                     @if ($item->reference()->first()->completed == 'Yes' && $item->reference()->first()->completedtwo == 'No')
                     <a href="/applications/export/exportReferee/{!! $item->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    @else
+                    <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
                     @endif
                     @if ($item->reference()->first()->completedtwo == 'Yes' && $item->reference()->first()->completed == 'No')
                     <a href="/applications/export/exportRefereeTwo/{!! $item->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
+                    @else
+                    <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
                     @endif
-
-                            {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
-
-
 
                 {{--@endforeach--}}
             </td>
