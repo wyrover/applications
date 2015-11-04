178a6c78f2fb1d0c47ffce8ecbd2ee183ed0d4ab
lstables <lstables1@gmail.com>
2015-11-04 10:17:27 +0000

    updates

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -86,7 +86,7 @@ class ApplicationsController extends Controller
     }
 
 
-    public function delete(Request $request)
+    public function destory(Request $request)
     {
         $id = $request->input('id');
         $apps = Applications::where('id', '=' , $id)->first();
