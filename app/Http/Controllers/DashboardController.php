5193bad98cc88f2193aeedc5b2105c39718529f4
lstables <lstables1@gmail.com>
2015-11-11 16:08:50 +0000

    ...

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,20 +19,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        if (App::environment('local')) {
-            $segment = \Request::url();
-            $search = ['http://', 'https://', '.applications', '.app', '/' . \Request::segment(1)];
-            $replace = ['', '', '', '', ''];
-            $output = str_replace($search, $replace, $segment);
-            $company = Company::where('user_id', Auth::user()->id);
-        }
-        if (App::environment('production')) {
-            $segment = \Request::url();
-            $search = ['http://', 'https://', '.madesimpleltd', '.co.uk', '/' . \Request::segment(1)];
-            $replace = ['', '', '', '', ''];
-            $output = str_replace($search, $replace, $segment);
-            $company = Company::where('user_id', Auth::user()->id);
-        }
+        $segment = \Request::url();
+        $search = ['http://', 'https://', '.madesimpleltd', '.co.uk', '/' . \Request::segment(1)];
+        $replace = ['', '', '', '', ''];
+        $output = str_replace($search, $replace, $segment);
+        $company = Company::where('user_id', Auth::user()->id);
 
         if (Auth::check() && Auth::user()->role != 'Admin' || $company == $output){
             return view('dashboard');
