f3b4d177a3a9b45b97bacf4a1109f34e3adc21a8
lstables <lstables1@gmail.com>
2015-11-25 12:17:38 +0000

    updates

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -53,16 +53,16 @@
 
                     {{--@endif--}}
                     {!! dd($item->RefereeOne($item->id, $item->reference()->first()->id))->first() !!}
-                    @if ($item->RefereeOne($item->id, $item->reference()->first()->id))
-                    <a href="/applications/export/exportReferee/{!! $item->reference()->first()->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-                    @else
-                    <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
-                    @endif
-                    @if ($item->reference()->first()->completed == 'Yes' && $item->reference()->first()->completed == 'No')
-                    <a href="/applications/export/exportRefereeTwo/{!! $item->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>
-                    @else
-                    <button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>
-                    @endif
+                    {{--@if ($item->RefereeOne())--}}
+                    {{--<a href="/applications/export/exportReferee/{!! $item->reference()->first()->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+                    {{--@else--}}
+                    {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
+                    {{--@endif--}}
+                    {{--@if ($item->reference()->first()->completed == 'Yes' && $item->reference()->first()->completed == 'No')--}}
+                    {{--<a href="/applications/export/exportRefereeTwo/{!! $item->id !!}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Reference"><i class="fa fa-download"></i> Download</a>--}}
+                    {{--@else--}}
+                    {{--<button class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Awaiting response from referee"><i class="fa fa-clock-o"></i></button>--}}
+                    {{--@endif--}}
 
                 {{--@endforeach--}}
             </td>
