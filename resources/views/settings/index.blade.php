162eef20a02672456abfa1cc916fd1ced7b44a83
lstables <lstables1@gmail.com>
2015-11-26 09:20:35 +0000

    updates

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -18,7 +18,7 @@
         <thead>
             <tr>
                 <th>Label</th>
-                <th>Hide Field?</th>
+                {{--<th>Hide Field?</th>--}}
                 <th>Mandatory</th>
                 <th>Type</th>
                 <th>Options (separate with a comma ,)</th>
@@ -29,10 +29,10 @@
 
             <tr>
                 <td><input type="text" name="label" class="form-control" value="{!! $setting->label !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide', false)  !!}
-                    {!! Form::checkbox('hide', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory', $setting->mandatory , $setting->mandatory, null) !!}</td>
                 <td>{!! Form::select('type', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options" class="form-control" value="{!! $setting->options !!}"></td>
@@ -40,10 +40,10 @@
 
             <tr>
                 <td><input type="text" name="label2" class="form-control" value="{!! $setting->label2 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide2', false)  !!}
-                    {!! Form::checkbox('hide2', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide2', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide2', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory2', $setting->mandatory2 , $setting->mandatory2, null) !!}</td>
                 <td>{!! Form::select('type2', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type2, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options2" class="form-control" value="{!! $setting->options2 !!}"></td>
@@ -51,9 +51,9 @@
 
             <tr>
                 <td><input type="text" name="label3" class="form-control" value="{!! $setting->label3 !!}"></td>
-                <td>{!!  Form::hidden('hide3', false)  !!}
-                   {!! Form::checkbox('hide3', true)  !!}
-                </td>
+                {{--<td>{!!  Form::hidden('hide3', false)  !!}--}}
+                   {{--{!! Form::checkbox('hide3', true)  !!}--}}
+                {{--</td>--}}
                 <td> {!! Form::checkbox('mandatory3', $setting->mandatory3 , $setting->mandatory3, null) !!}</td>
                 <td>{!! Form::select('type3', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type3, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options3" class="form-control" value="{!! $setting->options3 !!}"></td>
@@ -61,10 +61,10 @@
 
             <tr>
                 <td><input type="text" name="label4" class="form-control" value="{!! $setting->label4 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide4', false)  !!}
-                    {!! Form::checkbox('hide4', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide4', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide4', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory4', $setting->mandatory4 , $setting->mandatory4, null) !!}</td>
                 <td>{!! Form::select('type4', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type4, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options4" class="form-control" value="{!! $setting->options4 !!}"></td>
@@ -72,9 +72,9 @@
 
             <tr>
                 <td><input type="text" name="label5" class="form-control" value="{!! $setting->label5 !!}"></td>
-                <td>{!!  Form::hidden('hide5', false)  !!}
-                {!! Form::checkbox('hide5', true)  !!}
-                </td>
+                {{--<td>{!!  Form::hidden('hide5', false)  !!}--}}
+                {{--{!! Form::checkbox('hide5', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory5', $setting->mandatory5 , $setting->mandatory5, null) !!}</td>
                 <td>{!! Form::select('type5', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type5, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options5" class="form-control" value="{!! $setting->options5 !!}"></td>
@@ -82,10 +82,10 @@
 
             <tr>
                 <td><input type="text" name="label6" class="form-control" value="{!! $setting->label6 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide6', false)  !!}
-                    {!! Form::checkbox('hide6', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide6', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide6', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory6', $setting->mandatory6 , $setting->mandatory6, null) !!}</td>
                 <td>{!! Form::select('type6', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type6, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options6" class="form-control" value="{!! $setting->options6 !!}"></td>
@@ -93,10 +93,10 @@
 
             <tr>
                 <td><input type="text" name="label7" class="form-control" value="{!! $setting->label7 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide7', false)  !!}
-                    {!! Form::checkbox('hide7', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide7', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide7', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory7', $setting->mandatory7 , $setting->mandatory7, null) !!}</td>
                 <td>{!! Form::select('type7', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type7, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options7" class="form-control" value="{!! $setting->options7 !!}"></td>
@@ -104,10 +104,10 @@
 
             <tr>
                 <td><input type="text" name="label8" class="form-control" value="{!! $setting->label8 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide8', false)  !!}
-                    {!! Form::checkbox('hide8', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide8', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide8', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory8', $setting->mandatory8 , $setting->mandatory8, null) !!}</td>
                 <td>{!! Form::select('type8', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type8, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options8" class="form-control" value="{!! $setting->options8 !!}"></td>
@@ -115,10 +115,10 @@
 
             <tr>
                 <td><input type="text" name="label9" class="form-control" value="{!! $setting->label9 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide9', false)  !!}
-                    {!! Form::checkbox('hide9', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide9', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide9', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory9', $setting->mandatory9 , $setting->mandatory9, null) !!}</td>
                 <td>{!! Form::select('type9', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type9, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options9" class="form-control" value="{!! $setting->options9 !!}"></td>
@@ -126,10 +126,10 @@
 
             <tr>
                 <td><input type="text" name="label10" class="form-control" value="{!! $setting->label10 !!}"></td>
-                <td>
-                    {!!  Form::hidden('hide10', false)  !!}
-                    {!! Form::checkbox('hide10', true)  !!}
-                </td>
+                {{--<td>--}}
+                    {{--{!!  Form::hidden('hide10', false)  !!}--}}
+                    {{--{!! Form::checkbox('hide10', true)  !!}--}}
+                {{--</td>--}}
                 <td>{!! Form::checkbox('mandatory10', $setting->mandatory10 , $setting->mandatory10, null) !!}</td>
                 <td>{!! Form::select('type10', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $setting->type10, ['class' => 'form-control']) !!}</td>
                 <td><input type="text" name="options10" class="form-control" value="{!! $setting->options10 !!}"></td>
--- resources/views/settings/reference.blade.php
+++ resources/views/settings/reference.blade.php
@@ -10,7 +10,7 @@
     <thead>
         <tr>
             <th>Label</th>
-            <th>Hide Field?</th>
+            {{--<th>Hide Field?</th>--}}
             <th>Mandatory</th>
             <th>Type</th>
             <th>Options (separate with a comma ,)</th>
@@ -21,10 +21,10 @@
 
     <tr>
         <td><input type="text" name="label" class="form-control" value="{!! $ref->label !!}"></td>
-        <td>
-            {!!  Form::hidden('hide', false)  !!}
-            {!! Form::checkbox('hide', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide', false)  !!}--}}
+            {{--{!! Form::checkbox('hide', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory', $ref->mandatory , $ref->mandatory, null) !!}</td>
         <td>{!! Form::select('type', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options" class="form-control" value="{!! $ref->options !!}"></td>
@@ -32,10 +32,10 @@
 
     <tr>
         <td><input type="text" name="label2" class="form-control" value="{!! $ref->label2 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide2', false)  !!}
-            {!! Form::checkbox('hide2', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide2', false)  !!}--}}
+            {{--{!! Form::checkbox('hide2', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory2', $ref->mandatory2 , $ref->mandatory2, null) !!}</td>
         <td>{!! Form::select('type2', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type2, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options2" class="form-control" value="{!! $ref->options2 !!}"></td>
@@ -43,9 +43,9 @@
 
     <tr>
         <td><input type="text" name="label3" class="form-control" value="{!! $ref->label3 !!}"></td>
-        <td>{!!  Form::hidden('hide3', false)  !!}
-            {!! Form::checkbox('hide3', true)  !!}
-        </td>
+        {{--<td>{!!  Form::hidden('hide3', false)  !!}--}}
+            {{--{!! Form::checkbox('hide3', true)  !!}--}}
+        {{--</td>--}}
         <td> {!! Form::checkbox('mandatory3', $ref->mandatory3 , $ref->mandatory3, null) !!}</td>
         <td>{!! Form::select('type3', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type3, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options3" class="form-control" value="{!! $ref->options3 !!}"></td>
@@ -53,10 +53,10 @@
 
     <tr>
         <td><input type="text" name="label4" class="form-control" value="{!! $ref->label4 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide4', false)  !!}
-            {!! Form::checkbox('hide4', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide4', false)  !!}--}}
+            {{--{!! Form::checkbox('hide4', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory4', $ref->mandatory4 , $ref->mandatory4, null) !!}</td>
         <td>{!! Form::select('type4', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type4, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options4" class="form-control" value="{!! $ref->options4 !!}"></td>
@@ -64,9 +64,9 @@
 
     <tr>
         <td><input type="text" name="label5" class="form-control" value="{!! $ref->label5 !!}"></td>
-        <td>{!!  Form::hidden('hide5', false)  !!}
-            {!! Form::checkbox('hide5', true)  !!}
-        </td>
+        {{--<td>{!!  Form::hidden('hide5', false)  !!}--}}
+            {{--{!! Form::checkbox('hide5', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory5', $ref->mandatory5 , $ref->mandatory5, null) !!}</td>
         <td>{!! Form::select('type5', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type5, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options5" class="form-control" value="{!! $ref->options5 !!}"></td>
@@ -74,10 +74,10 @@
 
     <tr>
         <td><input type="text" name="label6" class="form-control" value="{!! $ref->label6 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide6', false)  !!}
-            {!! Form::checkbox('hide6', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide6', false)  !!}--}}
+            {{--{!! Form::checkbox('hide6', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory6', $ref->mandatory6 , $ref->mandatory6, null) !!}</td>
         <td>{!! Form::select('type6', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type6, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options6" class="form-control" value="{!! $ref->options6 !!}"></td>
@@ -85,10 +85,10 @@
 
     <tr>
         <td><input type="text" name="label7" class="form-control" value="{!! $ref->label7 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide7', false)  !!}
-            {!! Form::checkbox('hide7', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide7', false)  !!}--}}
+            {{--{!! Form::checkbox('hide7', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory7', $ref->mandatory7 , $ref->mandatory7, null) !!}</td>
         <td>{!! Form::select('type7', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type7, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options7" class="form-control" value="{!! $ref->options7 !!}"></td>
@@ -96,10 +96,10 @@
 
     <tr>
         <td><input type="text" name="label8" class="form-control" value="{!! $ref->label8 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide8', false)  !!}
-            {!! Form::checkbox('hide8', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide8', false)  !!}--}}
+            {{--{!! Form::checkbox('hide8', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory8', $ref->mandatory8 , $ref->mandatory8, null) !!}</td>
         <td>{!! Form::select('type8', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type8, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options8" class="form-control" value="{!! $ref->options8 !!}"></td>
@@ -107,10 +107,10 @@
 
     <tr>
         <td><input type="text" name="label9" class="form-control" value="{!! $ref->label9 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide9', false)  !!}
-            {!! Form::checkbox('hide9', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide9', false)  !!}--}}
+            {{--{!! Form::checkbox('hide9', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory9', $ref->mandatory9 , $ref->mandatory9, null) !!}</td>
         <td>{!! Form::select('type9', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type9, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options9" class="form-control" value="{!! $ref->options9 !!}"></td>
@@ -118,10 +118,10 @@
 
     <tr>
         <td><input type="text" name="label10" class="form-control" value="{!! $ref->label10 !!}"></td>
-        <td>
-            {!!  Form::hidden('hide10', false)  !!}
-            {!! Form::checkbox('hide10', true)  !!}
-        </td>
+        {{--<td>--}}
+            {{--{!!  Form::hidden('hide10', false)  !!}--}}
+            {{--{!! Form::checkbox('hide10', true)  !!}--}}
+        {{--</td>--}}
         <td>{!! Form::checkbox('mandatory10', $ref->mandatory10 , $ref->mandatory10, null) !!}</td>
         <td>{!! Form::select('type10', ['' => 'Please Select', 'input' => 'Text', 'textarea' => 'Textarea', 'select' => 'Select'], $ref->type10, ['class' => 'form-control']) !!}</td>
         <td><input type="text" name="options10" class="form-control" value="{!! $ref->options10 !!}"></td>
