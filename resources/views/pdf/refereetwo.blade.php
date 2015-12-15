ff88f952574f17452640aaadd4e942bb24c40e55
lstables <lstables1@gmail.com>
2015-12-15 10:45:30 +0000

    Updates

--- resources/views/pdf/refereetwo.blade.php
+++ resources/views/pdf/refereetwo.blade.php
@@ -127,7 +127,7 @@
         <td>
             Relationship: @if(! empty( $ref->referee_relationship)) <strong>{!! $ref->referee_relationship!!}</strong> @else <span class="no-answer">No answer given</span> @endif<br />
             Is this your previous /current employer?: @if(! empty($ref->referee_current_employer)) <strong>{!! $ref->referee_current_employer !!}</strong> @else <span class="no-answer">No answer given</span> @endif<br />
-            Can we contact this reference?: @if(! empty($ref->referee_contact)) <strong>{!! $ref->referee_contact !!}</strong> @else <span class="no-answer">No answer given</span> @endif
+            Can we contact this reference?: @if(! empty($ref->referee_contact2)) <strong>{!! $ref->referee_contact2 !!}</strong> @else <span class="no-answer">No answer given</span> @endif
         </td>
     </tr>
 
