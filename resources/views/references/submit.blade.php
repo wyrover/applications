d8f00be794e3acf99837c1d554e3efa08f8f18f5
lstables <lstables1@gmail.com>
2015-12-15 09:26:37 +0000

    updates

--- resources/views/references/submit.blade.php
+++ resources/views/references/submit.blade.php
@@ -73,7 +73,7 @@
                         <div class="form-group">
                             <div class="col-sm-4">Your Name</div>
                             <div class="col-lg-7">
-                                <input type="text" name="name" class="form-control" value="{!! $referee->referee_name !!}" autocomplete="off">
+                                <input type="text" name="name" class="form-control" value="" autocomplete="off">
                             </div>
                         </div>
 
