812379e6bf72bbd69a77a950124957b462b185c3
lstables <lstables1@gmail.com>
2015-12-18 13:35:07 +0000

    notifications

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -29,7 +29,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/admin/dashboard">Admin</a>
+            <a class="navbar-brand" href="https://admin.madesimpleapp.co.uk/dashboard">Admin</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
